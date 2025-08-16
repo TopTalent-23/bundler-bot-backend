5dc0017d356067157bcc3a3929a4c14da20ec56c
Toptalent-23 <mathieusleger4@gmail.com>
2025-08-15 21:18:21 -0300

    test with the previous version

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,26 +3,26 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
     {
-        telegramId: String,
+        telegramUserId: { type: Number, required: true, unique: true },
         username: String,
+        solanaWallet: {
+            publicKey: { type: String, required: true, unique: true },
+            privateKey: { type: String, required: true, unique: true }, // 🔐 Encrypt this in production
+        },
         fundWallet: {
-            publicKey: String,
-            privateKey: String, // 🔐 Encrypt this in production
+            publicKey: { type: String, required: true, unique: true },
+            privateKey: { type: String, required: true, unique: true }, // 🔐 Encrypt this in production
         },
         devWallet: {
-            publicKey: String,
-            privateKey: String, // 🔐 Encrypt this in production
+            publicKey: { type: String, required: true, unique: true },
+            privateKey: { type: String, required: true, unique: true }, // 🔐 Encrypt this in production
         },
         subWalletList: {
-            publicKeyList: String,
-            privateKeyList: String, // 🔐 Encrypt this in production
+            publicKeyList: { type: String, required: true, unique: true },
+            privateKeyList: { type: String, required: true, unique: true }, // 🔐 Encrypt this in production
         },
         role: { type: String, enum: ['user', 'admin'], default: 'user' },
         isVerified: { type: Boolean, default: false },
-        telegramUserId: { type: String, required: true, unique: true },
-        evmAddress: { type: String },
-        signature: { type: String },
-        solanaWallet: { type: String },
         language: { type: String, default: 'en' },
         createdAt: { type: Date, default: Date.now }
     },
